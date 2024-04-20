e96e4d20c084764ad95ca61a2a74fdb00e0e22e5
Royweru <royezi17@gmail.com>
2024-04-20 09:49:47 +0300

    modified:   components/product-description.tsx

--- components/product-description.tsx
+++ components/product-description.tsx
@@ -1,16 +1,13 @@
-import React from 'react'
-interface ProductDescriptionProps{
- content:any
+import React from "react";
+interface ProductDescriptionProps {
+  content: any;
 }
-export const ProductDescription = ({
- content
-}:ProductDescriptionProps) => {
+export const ProductDescription = ({ content }: ProductDescriptionProps) => {
   return (
-    <div className=' w-full font-bold tracking-wide text-white'>
-       <blockquote className=' text-2xl italic'>
-       {content.description}
-       
-       </blockquote>
+    <div className=" w-full font-bold tracking-wide text-white h-full justify-center p-12">
+      <blockquote className=" text-2xl italic">
+        {content.description}
+      </blockquote>
     </div>
-  )
-}
+  );
+};
