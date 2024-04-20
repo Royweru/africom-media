99148237a10cb4901a89d1791e631b33ad7b31df
Royweru <royezi17@gmail.com>
2024-04-20 10:45:47 +0300

    modified:   app/(routes)/gallery/components/gallery-art.tsx 	new file:   app/(routes)/gallery/components/images-display.tsx

--- app/(routes)/gallery/components/gallery-art.tsx
+++ app/(routes)/gallery/components/gallery-art.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GallleryHeader } from './gallery-header'
+import { ImagesDisplay } from './images-display'
 
 interface GalleryArtProps{
     data:any
@@ -8,6 +9,7 @@ export const GalleryArt = ({data}:GalleryArtProps) => {
   return (
     <div className=' flex flex-col gap-y-2 w-full h-full'>
         <GallleryHeader title={data.name} sub={data.description} />
+        <ImagesDisplay />
     </div>
   )
 }
---
+++ app/(routes)/gallery/components/images-display.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ImagesDisplay = () => {
+  return (
+    <div className=' w-full h-full px-9 '>
+        
+    </div>
+  )
+}
