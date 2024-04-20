7be7a92a44695de08190a09c8c2fd6770acfc39a
Royweru <royezi17@gmail.com>
2024-04-20 13:19:02 +0300

    modified:   components/cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -26,18 +26,13 @@ export const CTA= () => {
                             We help startups to grow and make money
                         </h2>
                         <p>
-                            Sed ut perspiciatis unde omnis iste natus voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
+                            Branding is simply a more efficient way to make sales
                         </p>
                         <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
                             <a href="javascript:void(0)" className="block py-2 px-4 text-center text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
                                 Let&#39;s get started
                             </a>
-                            <a href="javascript:void(0)" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
-                                Get access
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                    <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
-                                </svg>
-                            </a>
+                           
                         </div>
                     </div>
                     <div className="flex-none mt-14 md:mt-0 md:max-w-xl">
@@ -49,7 +44,7 @@ export const CTA= () => {
                     </div>
                 </div>
                 <div className="mt-14 px-4 md:px-8">
-                    <p className="text-center text-sm text-gray-700 font-semibold">Trusted by the best companies</p>
+                    <p className="text-center text-sm  text-sky-200 font-semibold">Trusted by the best companies</p>
                     <div className="flex justify-center items-center flex-wrap gap-x-12 gap-y-6 mt-6">
                         <div className=" w-[150px] h-[150px] rounded relative">
                             <Image
