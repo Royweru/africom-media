02bd7d98cb722d9d0604d643142df5eea2bafc3d
Royweru <royezi17@gmail.com>
2024-04-30 05:55:41 +0300

    modified:   app/globals.css 	modified:   app/page.tsx 	modified:   components/normal-carousel.tsx 	new file:   components/ui/button.tsx 	new file:   components/ui/card.tsx 	new file:   components/ui/carousel.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 
 import { CTA } from "@/components/cta";
+import { SlidingCarousel } from "@/components/normal-carousel";
 import { Services } from "@/components/services";
 import { SwipeCarousel } from "@/components/swipe-carousel";
 import { TextParallaxContentSection } from "@/components/text-parallax";
@@ -9,7 +10,7 @@ import VideoHero from "@/components/video-hero";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
-        <VideoHero />
+        <SlidingCarousel />
         <TextParallaxContentSection />
         <Services />
         <CTA />
--- components/normal-carousel.tsx
+++ components/normal-carousel.tsx
@@ -1,43 +1,90 @@
-// "use client"
-// import * as React from "react"
-// import Autoplay from "embla-carousel-autoplay"
+"use client";
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
 
-// import { Card, CardContent } from "@/components/ui/card"
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel"
+import { Card, CardContent } from "@/components/ui/card";
 
-// export function CarouselPlugin() {
-//   const plugin = React.useRef(
-//     Autoplay({ delay: 2000, stopOnInteraction: true })
-//   )
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
-//   return (
-//     <Carousel
-//       plugins={[plugin.current]}
-//       className="w-full max-w-xs"
-//       onMouseEnter={plugin.current.stop}
-//       onMouseLeave={plugin.current.reset}
-//     >
-//       <CarouselContent>
-//         {Array.from({ length: 5 }).map((_, index) => (
-//           <CarouselItem key={index}>
-//             <div className="p-1">
-//               <Card>
-//                 <CardContent className="flex aspect-square items-center justify-center p-6">
-//                   <span className="text-4xl font-semibold">{index + 1}</span>
-//                 </CardContent>
-//               </Card>
-//             </div>
-//           </CarouselItem>
-//         ))}
-//       </CarouselContent>
-//       <CarouselPrevious />
-//       <CarouselNext />
-//     </Carousel>
-//   )
-// }
+import Image from "next/image";
+export const ContentCarousel = [
+  {
+    id: 1,
+    imageUrl: "/images/all/9.jpg",
+    header: "We do branding",
+  },
+  {
+    id: 2,
+    imageUrl: "/images/all/12.jpg",
+    header: "We do branding",
+  },
+  {
+    id: 3,
+    imageUrl: "/images/all/15.jpg",
+    header: "We do branding",
+  },
+  {
+    id: 4,
+    imageUrl: "/images/all/16.jpg",
+    header: "We do branding",
+  },
+  {
+    id: 5,
+    imageUrl: "/images/all/10.jpg",
+    header: "We do branding",
+  },
+];
+
+export function SlidingCarousel() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
+
+  return (
+    <div className="w-full mb-2 h-full py-4 relative bg-white">
+      <Carousel
+        plugins={[plugin.current]}
+        className="relative w-full h-full"
+    
+      >
+         <div className="absolute top-0 left-0 w-full text-center">
+          <FadingText />
+        </div>
+        <CarouselContent className=" flex h-full  p-3">
+          {ContentCarousel.map((content) => (
+            <CarouselItem
+              key={content.id}
+              className=" flex-shrink-0 w-full rounded-xl"
+            >
+              <div className="w-full h-[450px] relative">
+                <Image
+                  fill
+                  src={content.imageUrl}
+                  alt=""
+                  className=" bg-cover bg-center rounded-xl "
+                />
+              </div>
+            </CarouselItem>
+            
+          ))}
+        </CarouselContent>
+      
+        <CarouselPrevious className="absolute top-1/2 left-0 transform -translate-y-1/2" />
+        <CarouselNext className="absolute top-1/2 right-0 transform -translate-y-1/2" />
+      </Carousel>
+    </div>
+  );
+}
+function FadingText() {
+    return (
+      <div className="text-white text-3xl font-bold">
+        <div className="fadeInOut">Stylish Fading Text</div>
+      </div>
+    )
+  }
